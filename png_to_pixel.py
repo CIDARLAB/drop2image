adeb4d29baf35ffee3c55d14c917d0bc753b6533
schuyler1007 <kkawata@bu.edu>
2022-04-11 00:38:11 -0400

    code updated

--- png_to_pixel.py
+++ png_to_pixel.py
@@ -17,7 +17,7 @@ def get_closest_color(pix, color):
     # in what form color input will be given?
     # hex, rgb, anything
     closest_color = None
-    min_diff = sys.maxsize
+    min_diff = 100000
     pix = np.array(pix)
     for clr in color:
         clr = np.array(clr)
@@ -32,14 +32,15 @@ def get_closest_image(filename, color_set):
     #im = Image.open(filename, 'r')
     im = cv2.imread(filename)
     color_set_rgb = [ImageColor.getrgb(color) for color in color_set]
-
     h, w, c = im.shape
     im_out = np.zeros((h,w,c))
 
     for i in range(h):
         for j in range(w):
-            closest_color = get_closest_color(im[i,j], color_set_rgb)
-            im_out[i,j,:] = closest_color
+            b,g,r = im[i,j]
+            c_r, c_g, c_b = get_closest_color((r,g,b), color_set_rgb)
+            im_out[i,j,:] = (c_b, c_g, c_r)
+            # fixing the order of rgb
 
     return im_out
 
@@ -48,10 +49,31 @@ def convert_jpeg_to_pix(filename):
     w, h = im.size
     im = im.convert('RGB')
     pix_list = []
-    for i in range(w):
-        for j in range(h):
-            pix = image.getpixel((i,j))
-            pix_list.append(tuple(pix))
+    if w%2==0:
+        for i in range(h):
+            if i%2==0:
+                for j in range(w):
+                    pix = im.getpixel((j,i))
+                    pix_list.append(tuple(pix))
+            else:
+                for j in range(w):
+                    pix = im.getpixel((w-1-j,i))
+                    pix_list.append(tuple(pix))
+    else:
+        for i in range(h):
+            if i%2==0:
+                for j in range(w):
+                    pix = im.getpixel((w-1-j,i))
+                    pix_list.append(tuple(pix))
+            else:
+                for j in range(w):
+                    pix = im.getpixel((j,i))
+                    pix_list.append(tuple(pix))
+
+    # for i in range(w):
+    #     for j in range(h):
+    #         pix = image.getpixel((i,j))
+    #         pix_list.append(tuple(pix))
 
     return pix_list
 
@@ -62,20 +84,40 @@ def output_format(filename, pix):
     return
 
 # example function
-image = resize_image('image.jpeg', (10, 10))
-image.save('newest_image.jpeg')
+# image = resize_image('image.jpeg', (10, 10))
+# image.save('newest_image.jpeg')
+#
+# color_set = ['#FFFFFF', '#000000', '#FFEB3B'] # user input
+# im_out = get_closest_image('image.jpeg', color_set)
+#
+# #im_out.save('closest_image.jpeg')
+# cv2.imwrite('closest_image.jpeg', im_out)
+#
+# im_resized_out = get_closest_image('newest_image.jpeg', color_set)
+# cv2.imwrite('closest_resized_image.jpeg', im_resized_out)
+# closest_resized_pix = convert_jpeg_to_pix('closest_resized_image.jpeg')
+# output_format('closest_resized_pix.txt', closest_resized_pix)
 
-color_set = ['#FFFFFF', '#000000'] # user input
-im_out = get_closest_image('image.jpeg', color_set)
 
-#im_out.save('closest_image.jpeg')
-cv2.imwrite('closest_image.jpeg', im_out)
+# test with emoji
+resized_test = resize_image("test.png", (10,10))
+resized_test.save('resized_test.png')
+color_set = ['#FF0000', '#FFFFFF', '#000000']
+im_out = get_closest_image('resized_test.png', color_set)
+cv2.imwrite('closest_test.png', im_out)
 
-im_resized_out = get_closest_image('newest_image.jpeg', color_set)
-cv2.imwrite('closest_resized_image.jpeg', im_resized_out)
-closest_resized_pix = convert_jpeg_to_pix('closest_resized_image.jpeg')
-output_format('closest_resized_pix.txt', closest_resized_pix)
+closest_resized_pix = convert_jpeg_to_pix('closest_test.png')
+output_format('test.txt', closest_resized_pix)
 
+
+resized_odd = resize_image("test_odd.png", (15,15))
+resized_odd.save("resized_odd.png")
+color_set_odd = ['#FFFF00', '#FFFFFF', '#000000']
+im_out_odd = get_closest_image('resized_odd.png', color_set_odd)
+cv2.imwrite('closest_odd.png', im_out_odd)
+
+closest_resized_odd_pix = convert_jpeg_to_pix('closest_odd.png')
+output_format('test_odd.txt', closest_resized_odd_pix)
 
 # txt = open('image_pix.txt', 'a')
 #
