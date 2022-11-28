70ff224058e5c80589caf43d186d9c47c11a07ab
schuyler1007 <kkawata@bu.edu>
2022-11-28 02:53:07 -0500

    add HTML GUI

--- main/web/js/main.js
+++ main/web/js/main.js
@@ -1,16 +1,39 @@
-document.getElementById("button-name").addEventListener("click", ()=>{eel.get_random_name()}, false);
-document.getElementById("button-number").addEventListener("click", ()=>{eel.get_random_number()}, false);
-document.getElementById("button-date").addEventListener("click", ()=>{eel.get_date()}, false);
-document.getElementById("button-ip").addEventListener("click", ()=>{eel.get_ip()}, false);
+document.getElementById("black").onclick = function(){
+  document.querySelector(":root").style.setProperty('--current-color', 'black')
+};
+document.getElementById("blue").onclick = function(){
+  document.querySelector(":root").style.setProperty('--current-color', 'blue')
+};
+document.getElementById("red").onclick = function(){
+  document.querySelector(":root").style.setProperty('--current-color', 'red')
+};
+document.getElementById("green").onclick = function(){
+  document.querySelector(":root").style.setProperty('--current-color', 'green')
+};
+document.getElementById("yellow").onclick = function(){
+  document.querySelector(":root").style.setProperty('--current-color', 'yellow')
+};
+document.getElementById("pink").onclick = function(){
+  document.querySelector(":root").style.setProperty('--current-color', 'pink')
+};
+document.getElementById("cyan").onclick = function(){
+  document.querySelector(":root").style.setProperty('--current-color', 'cyan')
+};
+document.getElementById("white").onclick = function(){
+  document.querySelector(":root").style.setProperty('--current-color', 'white')
+};
 
-document.getElementById("blue").onclick = function(){this.style.backgroundColor = getComputedStyle(document.querySelector(':root')).getPropertyValue('--current-color');};
+var pixs = document.getElementById("pixs");
+pixs.onclick = function(evt){
+  var target = evt.target || event.srcElement;
+  if ((target.tagName || "").match(/td/i)){
+    target.style.backgroundColor = getComputedStyle(document.querySelector(':root')).getPropertyValue('--current-color');
+  }
+};
 
-function set_current_color(color){
-    document.querySelector(':root').style.setProperty('--current-color', color);
-    console.log(getComputedStyle(document.querySelector(':root')).getPropertyValue('--current-color'));
-}
+document.getElementById("fetch").addEventListener("click", ()=>{eel.fetch()}, false);
 
-eel.expose(prompt_alerts);
-function prompt_alerts(description) {
-  alert(description);
-}
+// black with white outline
+// button smaller
+// dark mode
+// mouse down and mouse click might help do the click and drag coloring
